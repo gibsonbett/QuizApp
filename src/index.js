746be3aba8d8e23f18bd2827d7fd037503413c6a
gibsonbett <gibson.bett@student.moringaschool.com>
2022-09-01 20:55:22 +0300

    attempt to debug an error in fetching data from server

--- src/index.js
+++ src/index.js
@@ -1,22 +1,62 @@
-document.addEventListener("DOMContentLoaded", () => {
+// const initialize= () => {
  
-    document.getElementById("get_questions").addEventListener("submit", (event)=> {
-        event.preventDefault()
-    
-    })
-    // const input=document.getElementById('helpId')
-        fetch("http://localhost:3000/results")
-        .then((res) => res.json())
-        .then((data) => analyseData(data))
-    
-})
-function analyseData(quizBank){
-    quizBank.forEach(quiz=>{
-    let div=document.getElementByClassName('question');
-    let h4=document.createElement('h4')
-    div.appendChild(h4)
-    h4.innerHTML=quiz.question;
-})
+//     let submit =document.getElementById("get_questions")
+//     submit.addEventListener("submit", (e)=> {
+//         e.preventDefault();
+//         const input=document.getElementById('input_section');
+
+//         fetch(`http://localhost:3000/results/${input.value}`)
+//         .then((res) => res.json())
+//         .then((data) => analyseData(data))
+//     })  
+// function analyseData(quizBank){
+//     let h4=document.getElementById('header');
+//     h4.innerHTML=quizBank.question;
+// }
+// }
+// document.addEventListener("DOMContentLoaded", initialize)
 
 
-}
+// const input = document.getElementById('btn');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+
+const init = () => {
+    const inputForm =document.getElementById('get_questions')
+  
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.getElementById('input_section');
+      fetch(`http://localhost:3000/results/${input.value}`)
+      .then(response => response.json())
+      .then(data => console.log(data));
+    });
+  
+    // function analyseData(clothes){
+//         const h4=document.getElementById('header')
+//          h4.innerHTML=quizBank.question;
+// li.innerHTML=`${results.question} ${results.question}`;
+//         // ul.appendChild(li)
+      
+      
+    //   }
+    
+    }
+  
+//   document.addEventListener('DOMContentLoaded', init);
+  
+  
+  
+  // document.getElementById('search_clothes').addEventListener('submit',searchClothes)
+  // function fetchData(){
+  //   fetch('http://localhost:3000/Clothes')
+  // .then(resp=>resp.json())
+  // .then(data=>searchClothes(data))
+  // }
+  // fetchData()
+  
+  // function searchClothes(){
+  
+  
+  // }
