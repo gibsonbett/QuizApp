fec78aee7dbc261a1af7dc8bbe5dcf47bd99e284
gibsonbett <gibson.bett@student.moringaschool.com>
2022-09-01 16:38:24 +0300

    create elements and append them to the parent divs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
  
-    document.getElementById("get_questions")
-    submit.addEventListener("submit", (event)=> {
+    document.getElementById("get_questions").addEventListener("submit", (event)=> {
         event.preventDefault()
-        getQuiz(event.target.value)
+    
     })
-
-    function getQuiz( ){
+    // const input=document.getElementById('helpId')
         fetch("http://localhost:3000/results")
         .then((res) => res.json())
-        .then((data) => console.log(data))
-    }
+        .then((data) => analyseData(data))
+    
 })
+function analyseData(quizBank){
+    quizBank.forEach(quiz=>{
+    let div=document.getElementByClassName('question');
+    let h4=document.createElement('h4')
+    div.appendChild(h4)
+    h4.innerHTML=quiz.question;
+})
+
+
+}
